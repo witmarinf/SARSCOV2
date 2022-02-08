942d25d53cb3aa4ef20e026b856c5953d5ecdea3
MW <59529458+witmarinf@users.noreply.github.com>
2022-02-08 22:04:12 +0100

    dodane  TablePowLastDay

---
+++ Controllers/TablePowLastDayController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SARSCOV2.Controllers
+{
+    public class TablePowLastDayController : Controller
+    {
+        // GET: TablePowLastDay
+        public ActionResult Index()
+        {
+            DataSet ds = new DataSet();
+            string constr = ConfigurationManager.ConnectionStrings["C2"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                string query = "SELECT x,b,c,d,e,f,g,h,i,j,k,l FROM TablePowLastDay";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = conn;
+
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        sda.Fill(ds);
+                    }
+                }
+            }
+            return View(ds);
+        }
+    }
+}
